49c072b4dcfce36133273a7e9b542fd452ab814e
osbowski <osobiesam@gmail.com>
2021-10-22 00:12:51 +0200

    add-currency-table-to-routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import AddCurrency from '../views/AddCurrency.vue';
+import CurrenciesTablePage from '../views/CurrenciesTablePage.vue'
 
 const routes = [
   {
     path: '/add',
     component:AddCurrency
   },
+  {
+    path:'/currencies',
+    component:CurrenciesTablePage
+  }
 ]
 
 const router = createRouter({
