344b4fb3d623bb2650985fa2663fc1bf89e09d6b
osbowski <osobiesam@gmail.com>
2021-10-22 00:01:31 +0200

    add-currencies-table-page-and-currencies-table-component

---
+++ src/components/CurrenciesTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <th>Currency</th>
+        <th>Exchange Rate</th>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
---
+++ src/views/CurrenciesTablePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>Actual exchange rates</h2>
+    <CurrenciesTable />
+</template>
+
+<script>
+import CurrenciesTable from '../components/CurrenciesTable.vue';
+export default {
+    components:{
+        CurrenciesTable
+    },
+    setup() {
+        
+    },
+}
+</script>
