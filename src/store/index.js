bbb9c360bd298b27c9d94bd9fed82a66473032bf
osbowski <osobiesam@gmail.com>
2021-10-21 22:59:17 +0200

    add-state-currencies

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    currencies:{
+      USD:null,
+      EUR:null,
+      GBP:null
+    }
   },
   mutations: {
   },
   actions: {
   },
-  modules: {
-  }
 })
