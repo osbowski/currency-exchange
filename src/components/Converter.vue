728e543c8c7ba99e4b11b65f91965147578610bf
osbowski <osobiesam@gmail.com>
2021-10-22 01:24:48 +0200

    add-converter-component

---
+++ src/components/Converter.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="convert">
+    <input type="text" placeholder="Value to convert" />
+    <select v-model="activeCurrency">
+        <option v-for="(value,name) in currencies" :value="name" :key="name">{{name}}</option>
+    </select>
+    <button>Convert</button>
+  </form>
+  <p>In PLN:</p>
+</template>
+
+<script>
+import { useStore } from "vuex";
+import {computed, ref} from 'vue'
+export default {
+  setup() {
+    const store = useStore();
+    const currencies = computed(()=>store.getters.getCurrencies)
+    const activeCurrency = ref(null)
+
+    const convert = ()=>{
+        console.log(activeCurrency.value)
+    }
+
+
+    return {
+        currencies,
+        activeCurrency,
+        convert
+    }
+  },
+};
+</script>
--- src/views/ConvertCurrency.vue
+++ src/views/ConvertCurrency.vue
@@ -1,9 +1,14 @@
 <template>
     <h2>Convert currency to PLN</h2>
+    <Converter />
 </template>
 
 <script>
+import Converter from '../components/Converter.vue';
 export default {
+    components:{
+        Converter
+    },
     setup() {
         
     },
