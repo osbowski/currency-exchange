5889f6aa06a88c4820f7e54ec91edf929b51e7c3
osbowski <osobiesam@gmail.com>
2021-10-22 09:06:48 +0200

    create-header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-  </div>
-  <h1>Hello Vue</h1>
-  <router-view/>
+  <TheHeader />
+  <router-view />
 </template>
 
-<style>
-</style>
+<script>
+import TheHeader from './components/Layout/TheHeader.vue'
+export default {
+  components:{
+    TheHeader
+  },
+  setup() {
+
+  },
+}
+</script>
+<style></style>
---
+++ src/components/Layout/TheHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="nav">
+      <div>Currencies Converter</div>
+    <router-link to="/add">Add exchange rate</router-link>
+    <router-link to="/currencies">Currencies View</router-link>
+    <router-link to="/converter">Convert to PLN</router-link>
+  </div>
+</template>
