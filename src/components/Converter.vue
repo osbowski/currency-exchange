1c7bba760bd6272cee13389d58faa6be34962352
osbowski <osobiesam@gmail.com>
2021-10-22 08:50:01 +0200

    add-converting-functionality

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,33 +1,44 @@
 <template>
   <form @submit.prevent="convert">
-    <input type="text" placeholder="Value to convert" />
+    <input
+      type="text"
+      placeholder="Value to convert"
+      v-model="valueToConvert"
+    />
     <select v-model="activeCurrency">
-        <option v-for="(value,name) in currencies" :value="name" :key="name">{{name}}</option>
+      <option v-for="(value, name) in currencies" :value="name" :key="name">{{
+        name
+      }}</option>
     </select>
     <button>Convert</button>
   </form>
-  <p>In PLN:</p>
+  <p>In PLN: {{ convertResult }}</p>
 </template>
 
 <script>
 import { useStore } from "vuex";
-import {computed, ref} from 'vue'
+import { computed, ref } from "vue";
 export default {
   setup() {
     const store = useStore();
-    const currencies = computed(()=>store.getters.getCurrencies)
-    const activeCurrency = ref(null)
+    const currencies = computed(() => store.getters.getCurrencies);
+    const activeCurrency = ref(null);
+    const valueToConvert = ref(null);
+    const convertResult = ref(0);
 
-    const convert = ()=>{
-        console.log(activeCurrency.value)
-    }
-
+    const convert = () => {
+      convertResult.value = (
+        valueToConvert.value * currencies.value[activeCurrency.value]
+      ).toFixed(2);
+    };
 
     return {
-        currencies,
-        activeCurrency,
-        convert
-    }
+      currencies,
+      activeCurrency,
+      valueToConvert,
+      convertResult,
+      convert,
+    };
   },
 };
 </script>
